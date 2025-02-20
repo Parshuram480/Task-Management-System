bce211d9b2c7b56bb6851b6a79175c5037307924
Parshuram480 <parshurams.itpath@gmail.com>
2025-02-20 17:23:58 +0530

    authentication done

---
+++ task_management_system/usertask/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from usertask.views import *
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path('home/', home, name="home"),
+    # path('admin/', admin.site.urls),
+    path('login/', login_page, name="login"),
+    path('register/', register_page, name="register"),
+    path('home/', logout_view, name="logout")
+]
---
+++ task_management_system/usertask/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import *
+# from django.http import HttpResponse
+# from django.template import loader
+
+
+# Define view function for the Home page
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+
+def login_page(request):
+    # CHeck the method is POST
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get("password")
+
+        # check the user exists or not with provided username
+        if not User.objects.filter(username=username).exists():
+            messages.error(request, "Invalid USer")
+            return redirect('/login/')
+
+        # AUthenticate the USER
+        user = authenticate(username=username, password=password)
+
+        # Checking the password is matched or not
+        if user is None:
+            messages.error(request, "Invalid Password")
+            return redirect('/login/')
+        else:
+            login(request, user)
+            return redirect('/home/')
+
+    return render(request, "login.html")
+
+
+def register_page(request):
+    # check the request method is post
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # check that the user exists with the same username
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.info(request, "Username already taken.")
+            return redirect('/register/')
+
+        # creating the user
+        user = User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username
+        )
+
+        # saving the password in the user
+        user.set_password(password)
+        user.save()
+
+        messages.info(request, "account created successfully!")
+        return redirect('/login/')
+
+    return render(request, "register.html")
+
+
+def logout_view(request):
+    logout(request)
